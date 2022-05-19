b38a12f89434fa79e7da903e884d6dddb418c55f
654947306@qq.com <Pxy12081217>
2022-05-19 22:03:51 +0800

    修改信息

--- app/src/main/java/com/erradns/Activity/HomeActivity.java
+++ app/src/main/java/com/erradns/Activity/HomeActivity.java
@@ -1,20 +1,28 @@
 package com.erradns.Activity;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.erradns.Activity.home.ServerActivity;
 import com.erradns.Adapter.ViewPager2Adapter;
 import com.erradns.Fragment.Fragment_home;
 import com.erradns.Fragment.Fragment_message;
 import com.erradns.Fragment.Fragment_mine;
 import com.erradns.Sophix.R;
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
+import com.yw.game.floatmenu.FloatItem;
+import com.yw.game.floatmenu.FloatLogoMenu;
+import com.yw.game.floatmenu.FloatMenuView;
 
 import java.util.ArrayList;
 
@@ -25,7 +33,11 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
     private Fragment fragment_message;
     private Fragment fragment_mine;
     private ViewPager2 viewPager2;
+    private FloatLogoMenu mFloatMenu;//悬浮菜单
+    ArrayList<FloatItem> itemList = new ArrayList<>();
     ArrayList<Fragment> fragments = new ArrayList<>();
+    private int[] menuIcons = new int[]{R.drawable.addtask};
+    String[] MENU_ITEMS = {"下单"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +45,49 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
         setContentView(R.layout.activity_home);
 
         initData();
+        FloatMenuView();
         initView();
         initPager();
+    }
+
+    private void FloatMenuView() {
+        for (int i = 0; i < menuIcons.length; i++) {
+            itemList.add(new FloatItem(MENU_ITEMS[i], 0x99000000, 0x99000000, BitmapFactory.decodeResource(this.getResources(), menuIcons[i]), String.valueOf(i + 1)));
+        }
+        if (mFloatMenu == null) {
+            mFloatMenu = new FloatLogoMenu.Builder()
+                    .withActivity(this)
+//                    .withContext(mActivity.getApplication())//这个在7.0（包括7.0）以上以及大部分7.0以下的国产手机上需要用户授权，需要搭配<uses-permission android:name="android.permission.SYSTEM_ALERT_WINDOW"/>
+                    .logo(BitmapFactory.decodeResource(getResources(), R.drawable.errand))
+                    .drawCicleMenuBg(true)
+                    .backMenuColor(0xffe4e3e1)
+                    .setBgDrawable(this.getResources().getDrawable(R.color.wait))
+                    //这个背景色需要和logo的背景色一致
+                    .setFloatItems(itemList)
+                    .defaultLocation(FloatLogoMenu.RIGHT)
+                    .drawRedPointNum(false)
+                    .showWithListener(new FloatMenuView.OnMenuClickListener() {
+                        @Override
+                        public void onItemClick(int position, String title) {
+//                            Toast.makeText(HomeActivity.this, "position " + position + " title:" + title + " is clicked.", Toast.LENGTH_SHORT).show();
+//                            Log.i(TAG, "onItemClick: "+position);
+                            switch (position){
+                                case 0:
+                                    Intent server=new Intent(HomeActivity.this, ServerActivity.class);
+                                    startActivity(server);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+
+                        @Override
+                        public void dismiss() {
+
+                        }
+                    });
+        }
+
     }
 
     private void initData() {
